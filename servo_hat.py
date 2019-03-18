7fceda1a4bb298abbd178d5ba74773ed1ba9893d
Yoddumne <yoddumne@users.noreply.github.com>
2019-03-18 16:32:43 -0700

    add python code to repository

---
+++ servo_hat.py
@@ -0,0 +1,18 @@
+# Code by Jason Yoddumnern
+# allowed for reuse blah blah blah
+# Last Updated 2019 March 18
+# 
+# Code to control Servo HAT utilising up to 16 channels
+
+import time
+from adafruit_servokit import ServoKit
+
+# Kit used: servo HAT
+kit = ServoKit(channels=16)
+
+kit.servo[0].angle = 180
+time.sleep(1)
+
+kit.servo[0].angle = 0
+time.sleep(1)
+
